797b1a17a191ded9db32fe05de9a0eae19af487f
AndriusGelezevicius <andriusgelezevicius@gmail.com>
2023-06-19 12:30:42 +0300

    Created method to show MainWindow

--- app/src/main/java/com/andrius/library_manager/MainActivity.java
+++ app/src/main/java/com/andrius/library_manager/MainActivity.java
@@ -2,13 +2,26 @@ package com.andrius.library_manager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int DELAY_TIME = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, MainWindow.class);
+                startActivity(intent);
+                finish();
+            }
+        }, DELAY_TIME);
     }
 }
